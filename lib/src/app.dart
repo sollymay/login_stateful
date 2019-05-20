cfc7fffa679878b00f2e9d1d9f380d8c6720f0ed
Salomon May <sollymay@gmail.com>
2019-05-20 01:47:53 -0400

    added changes from last file

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'screens/login_screen.dart';
 class App extends StatelessWidget {
   Widget build(context) {
     return MaterialApp(
       title: 'Log me in!',
       home: Scaffold(
-        body: Text('SHow a form here'),
+        body: LoginScreen(),
       )
     );
   }
---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+   createState() {
+     return new LoginScreenState();
+   }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Text('Helloooo'),
+    );
+  }
+}
