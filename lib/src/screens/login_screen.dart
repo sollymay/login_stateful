a0f85cff3640a6c2e5f091a6916db7b677b08688
Salomon May Korn <salomon_may@apple.com>
2019-05-20 18:11:53 -0400

    added login password and submit button

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -10,7 +10,43 @@ class LoginScreenState extends State<LoginScreen> {
   Widget build(context) {
     return Container(
       margin: EdgeInsets.all(20.0),
-      child: Text('Helloooo'),
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            emailField(),
+            passwordField(),
+            Container(margin: EdgeInsets.only(top: 25.0)),
+            submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+  Widget emailField(){
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'example@example.com'
+      ),
+      keyboardType: TextInputType.emailAddress,
+    );
+  }
+
+  Widget passwordField(){
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: 'password',
+      ),
+      obscureText: true,
+    );
+  }
+
+  Widget submitButton(){
+    return RaisedButton(
+      color: Colors.blue[400],
+      onPressed:() {print('hello');},
+      child: Text('Submit'),
     );
   }
 }
