525e92998a177302dee338fda5b2cadc49d70f20
Salomon May <sollymay@gmail.com>
2019-05-19 23:29:44 -0400

    initial commit

---
+++ lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+      title: 'Log me in!',
+      home: Scaffold(
+        body: Text('SHow a form here'),
+      )
+    );
+  }
+}
